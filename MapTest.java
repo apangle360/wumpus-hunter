695d513c415edb95ad6716af8308150e19d46533
mcirillo415 <36200027+mcirillo415@users.noreply.github.com>
2018-02-07 05:09:58 -0500

    Add files via upload

--- MapTest.java
+++ MapTest.java
@@ -9,32 +9,26 @@ public class MapTest {
 
 	@Test
 	public void testMapNotNull() {
-		WumpusMap map = new WumpusMap(1,1);
+		WumpusMap map = new WumpusMap(1,0);
 		assertNotNull(map);
 	}
 	
 	@Test
 	public void testMakeMapOfTwoCaverns(){
-		WumpusMap smallestMap = new WumpusMap(1,1);
-		assertEquals(0, 1 - smallestMap.getNumberOfCaverns());
+		WumpusMap smallestMap = new WumpusMap(1,2);
+		assertEquals(0, 2 - smallestMap.getNumberOfCaverns());
 	}
 	
 	@Test
-	public void testTopRowOfBoundariesIdentified(){
+	public void testBoundaryIdentification(){
 		WumpusMap boundedMap = new WumpusMap(5,5);
 		assertEquals(0, 25 - boundedMap.getNumberOfCaverns());
-		for (int i = 0; i < 5; i++)
-			boundedMap.setBoundaryCavern(i, 4);
-		for (int xIndex = 0; xIndex < 5; xIndex++){
-			for (int yIndex = 0; yIndex < 5; yIndex++){
-				if (yIndex == 4){
-					assertEquals(true, boundedMap.getCaverns()[xIndex][yIndex].getIsBoundary());
-				} else{
-					assertEquals(false, boundedMap.getCaverns()[xIndex][yIndex].getIsBoundary());
-				}
-			}
-		}
 	}
-	
-	
+	@Test
+	public void testPlayerLocation() {
+		WumpusMap playerLocalTestMap = new WumpusMap(5,5);
+		playerLocalTestMap[4][2].setPlayerLocation;
+		assertTrue(playerLocalTestMap[4][2]);
+	}
+		
 }
--- WumpusMap.java
+++ WumpusMap.java
@@ -3,15 +3,8 @@ package main;
 public class WumpusMap {
 	private Integer numberOfCaverns = 0;
 	private Cavern[][] caverns;
-	private Integer playerX;
-	private Integer playerY;
-	private Integer WumpusX;
-	private Integer WumpusY;
 	
 	public WumpusMap(Integer xSize, Integer ySize){
-		this.playerX = 0;
-		this.playerY = 0;
-		numberOfCaverns = 0;
 		caverns = new Cavern[xSize][ySize];
 		for (int xIndex = 0; xIndex < xSize; xIndex++){
 			for (int yIndex = 0; yIndex < ySize; yIndex++){
@@ -19,35 +12,9 @@ public class WumpusMap {
 				numberOfCaverns++;
 			}
 		}
-		
 	}
 	
 	public Integer getNumberOfCaverns(){
 		return numberOfCaverns;
 	}
-	
-	public Cavern[][] getCaverns(){
-		return caverns;
-	}
-	
-	public void setBoundaryCavern(int boundaryX, int boundaryY){
-		caverns[boundaryX][boundaryY].setIsBoundary(true);
-	}
-	
-	public void setCavernHasPit(Cavern cavern){
-		cavern.setHasPit(true);
-	}
-	
-	public void setCavernHasBat(Cavern cavern){
-		cavern.setHasBat(true);
-	}
-	
-	public void setCavernHasArrow(Cavern cavern){
-		cavern.setHasArrow(true);
-	}
-	
-	public void setPlayerX(Integer playerX){
-		this.playerX = playerX;
-	}
-	
 }
