54a48f9a71429426a709d7d6d6d030aab6ff802b
mcirillo415 <36200027+mcirillo415@users.noreply.github.com>
2018-02-08 11:11:04 -0500

    Add files via upload

---
+++ ArrowDeath.java
@@ -0,0 +1,32 @@
+package fixtures;
+
+import main.Player;
+import main.WumpusMap;
+
+public class ArrowDeath {
+	private WumpusMap wumpusMap = new WumpusMap(5,5);
+	private WumpusMap testMap = wumpusMap.generateWumpusTestMap();
+	private char shootDirection;
+	private Player player = testMap.getPlayer();
+	
+	public void setPlayerX(int playerX) {
+		player.setPosX(playerX);
+		testMap.setPlayerArrows(5);
+	}
+
+	public void setPlayerY(int playerY) {
+		player.setPosY(playerY);
+	} 
+	
+	public void setDirection(char shootDirection) {
+		this.shootDirection = shootDirection;
+	}
+	
+	public String status(){
+		testMap.playerShootArrow(shootDirection);
+		if(testMap.playerDeathCheck()) return "You died";
+		return "nope";
+	}
+	
+}
+
